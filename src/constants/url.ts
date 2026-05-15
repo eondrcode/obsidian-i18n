10409cd979c11f2ae5dbd2f5cc25ae21d4318783
yun <144842921+yunrr@users.noreply.github.com>
2026-05-15 19:07:56 +0800

    update tutorial and documentation links

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,8 +2,8 @@ export default class Url {
     public static SPONSOR = "https://afdian.com/a/eondr"
     public static I18N_ICON = "https://raw.githubusercontent.com/eondrcode/obsidian-i18n/master/Obsidian.png"
     public static QQ_GROUP = "https://qm.qq.com/cgi-bin/qm/qr?k=kHTS0iC1FC5igTXbdbKzff6_tc54mOF5&jump_from=webapi&authKey=AoSkriW+nDeDzBPqBl9jcpbAYkPXN2QRbrMh0hFbvMrGbqZyRAbJwaD6JKbOy4Nx";
-    public static VIDEO_TUTORIAL = " https://www.bilibili.com/video/BV1VcxJeNExx/"
-    public static DOCUMENTATION_TUTORIAL = "https://github.com/eondrcode/obsidian-i18n"
+    public static VIDEO_TUTORIAL = "https://www.bilibili.com/video/BV13w5h6VEYS/"
+    public static DOCUMENTATION_TUTORIAL = "https://forum-zh.obsidian.md/t/topic/61546"
     public static TASK = "https://github.com/eondrcode/obsidian-i18n/issues"
     public static GITHUB_ISSUE = "https://github.com/eondrcode/obsidian-i18n/issues";
     public static SDK_URL = "https://download.immersivetranslate.com/immersive-translate-sdk-lite-latest.js"
